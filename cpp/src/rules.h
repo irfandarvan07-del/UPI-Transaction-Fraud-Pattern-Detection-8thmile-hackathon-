fecb7216f6795b378c12bbff0f4e03e7c0b434e6
MD IRFAN DARVAN <irfandarvan07@gmail.com>
2025-12-05 04:25:20 +0530

    Add RuleResult struct and rule functions in rules.h

--- cpp/src/rules.h
+++ cpp/src/rules.h
@@ -1,1 +1,16 @@
+#pragma once
+#include "csv_reader.h"
+#include <string>
+#include <vector>
+
+struct RuleResult {
+    bool suspicious_msg = false;
+    bool micro_cluster = false;
+    bool high_amount = false;
+    bool new_payee = false;
+    std::vector<std::string> reasons;
+};
+
+RuleResult apply_rules(const Txn &t);
+bool contains_keyword(const std::string &s);
 
